6506dc6418a06f8e18b1b690891f287312fcddfa
RonTheCoder50 <rohanvaybhase50@gmail.com>
2026-06-04 12:56:07 +0530

    fixing postgres dependency

--- HireTrack/backend/src/main/java/com/ron/backend/BackendApplication.java
+++ HireTrack/backend/src/main/java/com/ron/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.ron.backend;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
